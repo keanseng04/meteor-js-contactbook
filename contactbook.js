d1de6d6be700050328d65ad37f7bacdceb90a927
keanseng04 <keanseng04@gmail.com>
2015-05-30 15:18:59 +0800

    Halfway through the meteor workshop, just completed the delete section

---
+++ contactbook.js
@@ -0,0 +1,63 @@
+Contacts = new Mongo.Collection('contacts');
+
+var checked = [];
+
+if (Meteor.isClient) {
+  
+  // Contacts events
+  Template.contacts.helpers({
+    contacts: function() {
+      var filter = {sort: {}};
+      filter.sort[Session.get('sortby')] = 1;
+      return Contacts.find({}, filter);
+    }
+  });
+
+  // New Contact Templates
+  Template.new_contact.events({
+    "submit .contactForm" : function(event) {
+      
+      Contacts.insert({
+        name: event.target.name.value,
+        number: event.target.number.value,
+        email: event.target.email.value,
+        address: event.target.address.value,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = "";
+      event.target.number.value = "";
+      event.target.email.value = "";
+      event.target.address.value = "";
+      
+      $('#newContactModal').modal('toggle');
+    }
+  });
+
+  Template.contacts.events({
+    "click th": function(event){
+      var order = $(event.target).text().toLowerCase();
+      Session.set('sortby', order);
+    },
+
+    "click .delete": function () {
+      $.each(checked, function(index,value) {
+        Contacts.remove(value);
+      }); 
+    }
+  });
+
+  Template.contact.events({
+    "click .toggle-checked": function () {
+      var index = checked.indexOf(this._id);
+      if (index > -1) {
+        checked.splice(index, 1);
+      }
+      else {
+        checked.push(this._id);
+      }
+    }
+  });
+}
+
+
